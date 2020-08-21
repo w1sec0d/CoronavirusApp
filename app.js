bbaceb10551ac6f95363cf615eb786a44dda822a
w1sec0d <cadavid4003@gmail.com>
2020-08-21 12:13:01 -0500

    Better project folder organization

--- app.js
+++
@@ -1,8 +0,0 @@
-var index = document.getElementById("index");
-
-var ninos = document.getElementById("nino");
-ninos.addEventListener("click",modonino);
-
-function modonino(){
-    index.style.display = "none";
-}
